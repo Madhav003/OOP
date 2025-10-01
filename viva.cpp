5957e8426a3f1367843e9398f4253a79a62a8423
Madhav003 <madhavbhalodia2006@gmail.com>
2025-10-01 11:35:55 +0530

    viva

--- viva.cpp
+++ viva.cpp
@@ -1,35 +1,68 @@
+//make  a bank class with name balance and acc no. 
+//make default constructor to initialize aoj with name new and 0 0 for the other two. 
+//make a parameterized constructor to initialize all the three data members. 
+//make a function to display the details of the account. 
+//make a function to deposit an amount and another function to withdraw an amount. 
+//make a function to display balance. make a counter. there should be three objects and 1 extra object made with copy constructor
 #include <iostream>
 #include <string>
 using namespace std;
 
-class book {
-public:
+class Bank {
     string name;
-    float mrp;
-    float selling;
+    int accNo;
+    double balance;
+    static int counter;
+public:
+    Bank() {
+        name = "new";
+        accNo = 0;
+        balance = 0;
+        counter++;
+    }
+    Bank(string n, int a, double b) {
+        name = n;
+        accNo = a;
+        balance = b;
+        counter++;
+
+    }
+    Bank(const Bank &obj) {
+        name = obj.name;
+        accNo = obj.accNo;
+        balance = obj.balance;
+        counter++;
+    }
+    void display() {
+        cout << "Name: " << name << ", AccNo: " << accNo << ", Balance: " << balance << endl;
+    }
+    static void showCounter() {
+        cout << "Objects created: " << counter << endl;
+    }
+    ~Bank() {
+        cout << "Destructor called" << endl;
+        counter--;
+    }
 };
 
+int Bank::counter = 0;
+
 int main() {
-    book b1, b2;
+    Bank b1;
+    Bank b2("Madhav", 101, 5000);
+    Bank b3("Raza", 102, 3000);
+    Bank b4 = b2;
 
-    cout << "Enter name of book: ";
-    getline(cin, b2.name);
-    cout << "Enter MRP of book: ";
-    cin >> b2.mrp;
-    b2.selling = b2.mrp - (b2.mrp * 0.2);
-    cin.ignore();
-    cout << "Name: " << b2.name << endl;
-    cout << "MRP: " << b2.mrp << endl;
-    cout << "Selling price: " << b2.selling << endl;
+    b1.display();
+    b2.display();
+    b3.display();
+    b4.display();
 
-    cout << "Enter name of book: ";
-    getline(cin, b1.name);
-    cout << "Enter MRP of book: ";
-    cin >> b1.mrp;
-    b1.selling = b1.mrp - (b1.mrp * 0.2);
-    cout << "Name: " << b1.name << endl;
-    cout << "MRP: " << b1.mrp << endl;
-    cout << "Selling price: " << b1.selling << endl;
+    {
+        Bank b5("Temp", 103, 1000);
+        b5.display();
+    }
 
+    Bank::showCounter();
     return 0;
 }
