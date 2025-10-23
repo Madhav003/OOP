5e21037e29f71ed68d1b55be22b8ca07dfa4c3ab
Madhav003 <madhavbhalodia2006@gmail.com>
2025-10-23 22:45:50 +0530

    big lappy sync

---
+++ exp9/exp9a.cpp
@@ -0,0 +1,27 @@
+//a)write some simple code.	Create a base class 'Person' and a derived class 'Student' to demonstrate single inheritance.
+#include <iostream>
+using namespace std;
+class Person {
+    string name;
+    int age;
+public:
+    Person(string n, int a) : name(n), age(a) {}
+    void displayInfo() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+};
+
+class Student : public Person {
+    string major;
+public:
+    Student(string n, int a, string m) : Person(n, a), major(m) {}
+    void displayInfo() {
+        Person::displayInfo();
+        cout << "Major: " << major << endl;
+    }
+};
+int main() {
+    Student student("Alice", 20, "Computer Science");
+    student.displayInfo();
+    return 0;
+}
